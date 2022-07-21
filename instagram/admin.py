e5770338c576c2632c3f431cd5643a48b9475498
BONG <69753846+SSABOODA@users.noreply.github.com>
2022-07-21 23:26:35 +0900

    fixed untracked files

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import InstagramPost
 
-# Register your models here.
+"""
+Admin Resister 방법
+"""
+
+# 1. admin.site.register(InstagramPost)
+# 2. admin.site.register(InstagramPost, InstagramPostAdmin)
+# 3. @admin.register(InstagramPost)
+
+
+@admin.register(InstagramPost)
+class InstagramPostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'message',
+        'is_public',
+        'message_length',
+        'created_at',
+        'updated_at',
+    ]
+
+    list_display_links = [
+        'message',
+    ]
+
+    list_filter = [
+        'created_at',
+        'is_public',
+    ]
+
+    search_fields = [
+        'message',
+    ]
+
+    def message_length(self, post):
+        return f'{len(post.message)} 글자'
+    message_length.short_description = '메세지 글자수'
+
+
+
+
--- instagram/models.py
+++ instagram/models.py
@@ -1,7 +1,22 @@
-# from django.db import models
-#
-#
-# class Post(models.Model):
-#     message = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+from django.db import models
+
+
+class InstagramPost(models.Model):
+    message = models.TextField()
+    photo = models.ImageField(blank=True)
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'instagram_post'
+        verbose_name = 'Instagram 게시글'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f'({self.pk}) Instagram 게시글'
+
+    # 자주 쓰는 로직이라면 model에서 구현 아니라면 admin에서 구현
+    # def message_length(self):
+    #     return f'{len(self.message)}'
+    # message_length.short_description = '메세지 글자수'
